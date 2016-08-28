b3d2688ddee0801e067c5c61b2650d6fb184ee80
eddy5641 <eddydavinchi@gmail.com>
2016-08-27 19:28:27 -0700

    Nothing changed yet, project created

---
+++ Sightstone.AspNet/Sightstone.AspNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sightstone.AspNet.MvcApplication" Language="C#" %>
---
+++ Sightstone.AspNet/Sightstone.AspNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sightstone.AspNet.Startup))]
+namespace Sightstone.AspNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
