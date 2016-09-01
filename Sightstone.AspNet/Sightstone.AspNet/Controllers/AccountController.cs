8f88578274b74e8e1f5af2fb0990e06df0068a11
eddy5641 <eddydavinchi@gmail.com>
2016-09-01 11:38:21 -0700

    Start some work. It is decided

--- Sightstone.AspNet/Sightstone.AspNet/Controllers/AccountController.cs
+++ Sightstone.AspNet/Sightstone.AspNet/Controllers/AccountController.cs
@@ -120,7 +120,7 @@ namespace Sightstone.AspNet.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code,  model.RememberMe, model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -151,11 +151,17 @@ namespace Sightstone.AspNet.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                {
+                    UserName = model.Username,
+                    Email = model.Email
+                };
+                
+
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                    await SignInManager.SignInAsync(user, false, false);
                     
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
--- Sightstone.AspNet/Sightstone.AspNet/Models/AccountViewModels.cs
+++ Sightstone.AspNet/Sightstone.AspNet/Models/AccountViewModels.cs
@@ -69,6 +69,12 @@ namespace Sightstone.AspNet.Models
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
